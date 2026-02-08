db8672b58d5b1d83094f02c14bf3395447072caf
Lima-Ale <alelima0274@gmail.com>
2026-02-08 11:59:11 -0300

    Delete aula dia 07-02-26 directory

--- aula dia 07-02-26/ex01maioridade.js
+++
@@ -1,9 +0,0 @@
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
-
-const idade = Number(prompt("Digite a idade: "));
-
-if(idade >=18)
-    console.log('Ele(a) é maior de Idade');
-else
-    console.log('Ele(a) é menor de Idade');
